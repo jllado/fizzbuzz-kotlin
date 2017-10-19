54a0168e0734e4525a942b7b09bb87893c136f45
Juan Lladó <jllado@gmail.com>
2017-10-19 18:51:35 +0200

    In Kotlin I can avoid snake case for test names :-)

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -6,25 +6,25 @@ class FizzBuzzTest {
 
     @ParameterizedTest
     @ValueSource(ints = intArrayOf(1, 2, 4))
-    fun return_input(input: Int) {
+    fun `return input`(input: Int) {
         fizzBuzz(input) shouldEqual input.toString()
     }
 
     @ParameterizedTest
     @ValueSource(ints = intArrayOf(3, 6))
-    fun return_Fizz_when_input_multiple_of_three(input: Int) {
+    fun `return Fizz when input multiple of three`(input: Int) {
         fizzBuzz(input) shouldEqual "Fizz"
     }
 
     @ParameterizedTest
     @ValueSource(ints = intArrayOf(5, 10))
-    fun return_Buzz_when_input_multiple_of_five(input: Int) {
+    fun `return Buzz when input multiple of five`(input: Int) {
         fizzBuzz(input) shouldEqual "Buzz"
     }
 
     @ParameterizedTest
     @ValueSource(ints = intArrayOf(15, 30))
-    fun return_FizzBuzz_when_input_multiple_of_three_and_five(input: Int) {
+    fun `return FizzBuzz when input multiple of three and five`(input: Int) {
         fizzBuzz(input) shouldEqual "FizzBuzz"
     }
 
